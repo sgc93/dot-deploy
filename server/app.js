e95857a95d721dbf36d81398aa0085027d338e66
sgc93 <smachewgedefaw@gmail.com>
2024-11-02 00:35:43 -0900

    use prod vars

--- server/app.js
+++ server/app.js
@@ -26,15 +26,6 @@ app.use((req, res, next) => {
 	next();
 });
 
-// Optionally, add more headers if needed
-app.use((req, res, next) => {
-	res.header("Access-Control-Allow-Origin", frontendOrigin);
-	res.header("Access-Control-Allow-Credentials", "true");
-	res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-	res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-	next();
-});
-
 // middlewares
 app.use(morgan("dev"));
 app.use(express.json());
