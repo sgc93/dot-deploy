846d4450ad36f2793e7338f93f4e9e4bc46e2d78
sgc93 <smachewgedefaw@gmail.com>
2024-11-01 23:57:36 -0900

    use prod vars

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 
-const frontendOrigin = "http://localhost:5173";
+const frontendOrigin = "https://dot-code-nu.vercel.app";
 
 // middlewares
 app.use(cors({ origin: frontendOrigin, credentials: true }));
