f5d62938ae716ceba2a6b1f0b529010cad835268
sgc93 <smachewgedefaw@gmail.com>
2024-11-08 06:10:57 -0900

    use locals : developing mode

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 
-const frontedOrigin = process.env.PROD_CLIENT;
+const frontedOrigin = "http://localhost:5173";
 
 // middlewares
 app.use(
