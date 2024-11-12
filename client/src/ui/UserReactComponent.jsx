b37bc2b9714691f6132d593e433786cff86cd271
sgc93 <smachewgedefaw@gmail.com>
2024-11-12 04:37:36 -0900

    handling react output error

--- client/src/ui/UserReactComponent.jsx
+++ client/src/ui/UserReactComponent.jsx
@@ -1,32 +1,61 @@
 import * as Babel from "@babel/standalone";
+import { useEffect, useState } from "react";
 import JsxParser from "react-jsx-parser";
+
+const parseJsxError = (error) => {
+	const errorString = error.toString();
+	const match = errorString.match(/SyntaxError: (.+) \((\d+):(\d+)\)/);
+	if (match) {
+		const [, message, line, column] = match;
+
+		return { message, line, column, source: "component.jsx" };
+	}
+	return "Unknown error in JSX";
+};
 
 function isValidJSX(jsxString) {
 	try {
 		Babel.transform(jsxString, {
 			presets: ["react"],
 		});
-		return true; // No errors found
+		return null; // No errors found
 	} catch (error) {
-		console.error("Invalid JSX:", error.message);
-		return false; // Errors found
+		return parseJsxError(error); // Errors found
 	}
 }
 
-function UserReactComponent({ userJsx }) {
-	if (isValidJSX(userJsx)) {
+const UserReactComponent = ({ userJsx }) => {
+	const [err, setErr] = useState(isValidJSX(userJsx));
+
+	useEffect(() => {
+		setErr();
+	}, [userJsx]);
+
+	if (!err) {
 		return (
 			<div className="relative p-5">
 				<JsxParser
 					jsx={userJsx}
 					autoCloseVoidElements
-					onError={(error) => console.log("Rendering error:", error.message)}
+					onError={(error) =>
+						setErr({ message: `Rendering error: ${error.message}` })
+					}
 				/>
 			</div>
 		);
 	} else {
-		return <div className="">invalid jsx</div>;
+		return (
+			<div className="w-full relative p-5">
+				<div className="w-full flex items-center justify-center gap-2 bg-red-500 bg-opacity-20 border-2 border-red-500 p-5 rounded-md font-bold text-lg tracking-wider">
+					<span className="capitalize text-red-50">{err.message}</span>
+					<div className="flex items-center text-red-500">
+						{err.line && <span>line: {err.line}</span>}
+						{err.column && <span>, col: {err.column}</span>}
+					</div>
+				</div>
+			</div>
+		);
 	}
-}
+};
 
 export default UserReactComponent;
